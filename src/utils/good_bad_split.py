3f6e06176ada6f0ecbcf5bc110d4640ecedfa5cb
Nicolas Emiliani <nicolas@motrixi.com>
2015-02-08 16:37:59 -0300

    agrega script que mete var binaria buena/malo en funcion de quality

---
+++ src/utils/good_bad_split.py
@@ -0,0 +1,15 @@
+import pandas
+import argparse
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='agrega variable')
+    parser.add_argument('-f', '--data_file', type=str, help='data file')
+    parser.add_argument('-o', '--output_file', type=str, default='out.csv')
+    args = parser.parse_args()
+
+    df = pandas.read_csv(args.data_file, sep=';', header=0)
+    df['quality_2'] = df.apply(lambda x : 0 if x.get('quality') <= 5 else 1, axis=1)
+    df.to_csv(args.output_file, sep=';')
+
